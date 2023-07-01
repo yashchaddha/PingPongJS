d9e1097a0ded8f5afee808a5ea97c4038d90febf
Yash Chaddha <yash.chaddha1997@gmail.com>
2023-07-01 13:00:38 +0000

    PingPongGame

--- dist/styles.dd855970.js
+++ dist/styles.dd855970.js
@@ -117,123 +117,62 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.js":[function(require,module,exports) {
-var ball = document.getElementById('ball');
-var rod1 = document.getElementById('rod1');
-var rod2 = document.getElementById('rod2');
-var storeName = "PPName";
-var storeScore = "PPMaxScore";
-var rod1Name = "Rod 1";
-var rod2Name = "Rod 2";
-var score,
-  maxScore,
-  movement,
-  rod,
-  ballSpeedX = 2,
-  ballSpeedY = 2;
-var gameOn = false;
-var windowWidth = window.innerWidth,
-  windowHeight = window.innerHeight;
-(function () {
-  rod = localStorage.getItem(storeName);
-  maxScore = localStorage.getItem(storeScore);
-  if (rod === "null" || maxScore === "null") {
-    alert("This is the first time you are playing this game. LET'S START");
-    maxScore = 0;
-    rod = "Rod1";
-  } else {
-    alert(rod + " has maximum score of " + maxScore * 100);
+})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
-  resetBoard(rod);
-})();
-function resetBoard(rodName) {
-  rod1.style.left = (window.innerWidth - rod1.offsetWidth) / 2 + 'px';
-  rod2.style.left = (window.innerWidth - rod2.offsetWidth) / 2 + 'px';
-  ball.style.left = (windowWidth - ball.offsetWidth) / 2 + 'px';
-
-  // Lossing player gets the ball
-  if (rodName === rod2Name) {
-    ball.style.top = rod1.offsetTop + rod1.offsetHeight + 'px';
-    ballSpeedY = 2;
-  } else if (rodName === rod1Name) {
-    ball.style.top = rod2.offsetTop - rod2.offsetHeight + 'px';
-    ballSpeedY = -2;
-  }
-  score = 0;
-  gameOn = false;
+  return bundleURL;
 }
-function storeWin(rod, score) {
-  if (score > maxScore) {
-    maxScore = score;
-    localStorage.setItem(storeName, rod);
-    localStorage.setItem(storeScore, maxScore);
-  }
-  clearInterval(movement);
-  resetBoard(rod);
-  alert(rod + " wins with a score of " + score * 100 + ". Max score is: " + maxScore * 100);
-}
-window.addEventListener('keypress', function () {
-  var rodSpeed = 20;
-  var rodRect = rod1.getBoundingClientRect();
-  if (event.code === "KeyD" && rodRect.x + rodRect.width < window.innerWidth) {
-    rod1.style.left = rodRect.x + rodSpeed + 'px';
-    rod2.style.left = rod1.style.left;
-  } else if (event.code === "KeyA" && rodRect.x > 0) {
-    rod1.style.left = rodRect.x - rodSpeed + 'px';
-    rod2.style.left = rod1.style.left;
-  }
-  if (event.code === "Enter") {
-    if (!gameOn) {
-      gameOn = true;
-      var ballRect = ball.getBoundingClientRect();
-      var ballX = ballRect.x;
-      var ballY = ballRect.y;
-      var ballDia = ballRect.width;
-      var rod1Height = rod1.offsetHeight;
-      var rod2Height = rod2.offsetHeight;
-      var rod1Width = rod1.offsetWidth;
-      var rod2Width = rod2.offsetWidth;
-      movement = setInterval(function () {
-        // Move ball 
-        ballX += ballSpeedX;
-        ballY += ballSpeedY;
-        rod1X = rod1.getBoundingClientRect().x;
-        rod2X = rod2.getBoundingClientRect().x;
-        ball.style.left = ballX + 'px';
-        ball.style.top = ballY + 'px';
-        if (ballX + ballDia > windowWidth || ballX < 0) {
-          ballSpeedX = -ballSpeedX; // Reverses the direction
-        }
-
-        // It specifies the center of the ball on the viewport
-        var ballPos = ballX + ballDia / 2;
-
-        // Check for Rod 1
-        if (ballY <= rod1Height) {
-          ballSpeedY = -ballSpeedY; // Reverses the direction
-          score++;
-
-          // Check if the game ends
-          if (ballPos < rod1X || ballPos > rod1X + rod1Width) {
-            storeWin(rod2Name, score);
-          }
-        }
-
-        // Check for Rod 2
-        else if (ballY + ballDia >= windowHeight - rod2Height) {
-          ballSpeedY = -ballSpeedY; // Reverses the direction
-          score++;
-
-          // Check if the game ends
-          if (ballPos < rod2X || ballPos > rod2X + rod2Width) {
-            storeWin(rod1Name, score);
-          }
-        }
-      }, 10);
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+    if (matches) {
+      return getBaseURL(matches[0]);
     }
   }
-});
-},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  return '/';
+}
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+function updateLink(link) {
+  var newLink = link.cloneNode();
+  newLink.onload = function () {
+    link.remove();
+  };
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+var cssTimeout = null;
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+    cssTimeout = null;
+  }, 50);
+}
+module.exports = reloadCSS;
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/styles.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -402,5 +341,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
-//# sourceMappingURL=/src.a2b27638.js.map
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/styles.dd855970.js.map
